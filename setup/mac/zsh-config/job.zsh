06f60d2921b1a1dc46b16e2fc75f6b2e78f35491
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 13:07:03 -0400

    2024-Config-Update

---
+++ setup/mac/zsh-config/job.zsh
@@ -0,0 +1,4 @@
+# [Job Config]
+# --------------------------------------------------------------------------------------------------------
+export ADO_TOKEN="op://Development/ADO-Token/password"
+export B64_PAT="op://Development/ADO-Token/password"
---
+++ setup/mac/zsh-config/prompt.zsh
@@ -0,0 +1,8 @@
+# [Prompt]
+# --------------------------------------------------------------------------------------------------------
+autoload -U +X bashcompinit && bashcompinit
+
+eval "$(starship init zsh)"
+export STARSHIP_CONFIG=~/Developer/dotfiles-hd/config/starship/starship.toml
+eval "$(zoxide init --cmd cd zsh)"
+source ~/Developer/zsh-you-should-use/you-should-use.plugin.zsh
