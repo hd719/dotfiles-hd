ed2395bb07cfa0688d91b9e5b1ebe07c167b15d5
Hamel Desai <hameldesai93@gmail.com>
2025-12-05 09:48:56 -0500

    Update add conditional for nix

--- setup/mac-vm/zsh-config/prompt.zsh
+++ setup/mac-vm/zsh-config/prompt.zsh
@@ -5,13 +5,18 @@ export TERM=xterm-256color
 
 autoload -U +X bashcompinit && bashcompinit
 
-# Without Devbox:
-# eval "$(zoxide init --cmd cd zsh)"
-# eval "$(starship init zsh)"
+# Check if Nix/Devbox is installed and use appropriate paths
+if command -v nix &> /dev/null; then
+    # With Devbox: Use Nix-managed binaries
+    eval "$(/Users/hameldesai/.local/share/devbox/global/default/.devbox/nix/profile/default/bin/zoxide init --cmd cd zsh)"
+    eval "$(/Users/hameldesai/.local/share/devbox/global/default/.devbox/nix/profile/default/bin/starship init zsh)"
+else
+    # Without Devbox: Use system binaries
+    eval "$(zoxide init --cmd cd zsh)"
+    eval "$(starship init zsh)"
+fi
 
-# With Devbox: 
-eval "$(/Users/hameldesai/.local/share/devbox/global/default/.devbox/nix/profile/default/bin/zoxide init --cmd cd zsh)"
-eval "$(/Users/hameldesai/.local/share/devbox/global/default/.devbox/nix/profile/default/bin/starship init zsh)"
+
 export STARSHIP_CONFIG=~/Developer/dotfiles-hd/config/starship/starship.toml
 
 source ~/Developer/zsh-you-should-use/you-should-use.plugin.zsh
